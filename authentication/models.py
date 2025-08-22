9f9334fc559688b8d0864babdfb0a59aedadbe9e
john Doe <johndoe@example.com>
2025-08-22 14:22:20 +0200

    modificatins dans des fichiers

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    groups = models.ManyToManyField('auth.Group', related_name='custom_user_set', blank=True)
+
+    user_permissions = models.ManyToManyField('auth.Permission', related_name='custom_user_set', blank=True)
+
+    CREATOR = 'CREATOR'
+    SUBSCRIBER = 'SUBSCRIBER'
+
+    ROLE_CHOICES = [
+        (CREATOR, 'Creator'),
+        (SUBSCRIBER, 'Subscriber'),
+    ]
+    profile_photo = models.ImageField()
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from . import forms,models
 
-# Create your views here.
+@login_required
+def upload_photo(request):
+    form = forms.PhotoForm(request.POST, request.FILES)
+    if request.method == 'POST':
+        if form.is_valid():
+            photo = form.save(commit=False)
+            photo.uploader = request.user
+            photo.save()
+            return redirect('home')
+    return render(request, 'blog/upload_photo.html', {'form': form})
+
+
+@login_required
+def home(request):
+    photos = models.Photo.objects.all()
+    return render(request, 'blog/home.html', context={'photos': photos})
